bdfbab39199bc50825b748ccee69bf42632a4895
sandunrmst <sandunalugc@gmail.com>
2024-06-13 13:10:33 +0530

    build recipe list card and show list of menu items

--- src/app/recipe-list/page.js
+++ src/app/recipe-list/page.js
@@ -2,7 +2,7 @@ import RecipeList from "@/components/recipe-list";
 
 async function fetchListOfRecipes(){
     try{
-        const apiResponse =await fetch('https://dummyjson.com/products')
+        const apiResponse =await fetch('https://dummyjson.com/recipes')
         const data = await apiResponse.json()
 
         return data?.recipes 
