a784c69259d73d5285e006ca5310faa4d2e8d790
sandunrmst <sandunalugc@gmail.com>
2024-06-13 16:17:08 +0530

    Add image

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 
 export default async function Home() {
   return (
-    <div>
-      <h1>Welcome to Recipe App</h1>
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-[url('foodimg.jpg')] opacity-90">
+      <h1 className="text-white font-bold text-6xl shadow-md text-center">Welcome to MealMaster</h1>
+      <div className="mt-10 flex items-center gap-2 px-6 py-4 bg-orange-600 rounded-md text-2xl text-white font-bold hover:bg-orange-700 transition-all duration-500 ">
       <Link href={"/recipe-list"}>Explore Recipes</Link>
-
+      <FaRegArrowAltCircleRight className="transition-transform duration-500 hover:translate-x-2.5"/>
+      </div>
     </div>
   );
 }
