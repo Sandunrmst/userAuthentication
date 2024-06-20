5dbdcb438edb6dec764b90abded09eef76d3d5b4
sandunrmst <sandunalugc@gmail.com>
2024-06-20 17:28:16 +0530

    build common form element

---
+++ src/components/form-element/page.js
@@ -0,0 +1,32 @@
+function CommonFormElement({ currentItem, value, onChange }) {
+  let content = null;
+
+  switch (currentItem.componetType) {
+    case "input":
+      content = (
+        <Input
+          name={currentItem.name}
+          id={currentItem.name}
+          placeholder={currentItem.placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      );
+      break;
+    default:
+      content = (
+        <Input
+          name={currentItem.name}
+          id={currentItem.name}
+          placeholder={currentItem.placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      );
+      break;
+  }
+
+  return content;
+}
+
+export default CommonFormElement;
