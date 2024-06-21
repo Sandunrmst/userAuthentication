352ce069a20b3e59809734f74adcee79259bfbb5
sandunrmst <sandunalugc@gmail.com>
2024-06-21 14:48:10 +0530

    show currrent user details in welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
-import Link from "next/link";
-import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { fetchAuthUserAction } from "@/actions";
 
 export default async function Home() {
+  const currentUser = await fetchAuthUserAction();
+
   return (
     <div className="w-full min-h-screenflex">
       <h1 className="text-black font-bold text-6xl text-center">
         Welcome to Auth
       </h1>
+
+      <h3>{currentUser?.data?.userName}</h3>
+      <p>{currentUser?.data?.email}</p>
     </div>
   );
 }
